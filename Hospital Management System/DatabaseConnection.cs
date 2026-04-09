4183e266b4246b529a60c8962d59b69acb7a49f9
Conquerorr0 <fatihaltuntas0@outlook.com>
2026-04-09 15:31:48 +0300

    feat: Database connected

---
+++ Hospital Management System/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Hospital_Management_System
+{
+    class DatabaseConnection
+    {
+        //Data Source=kerem\SQLEXPRESS;Initial Catalog=HospitalManagementSystem;Integrated Security=True;Trust Server Certificate=True
+        public SqlConnection connection()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=kerem\\SQLEXPRESS;Initial Catalog=HospitalManagementSystem;Integrated Security=True;Trust Server Certificate=True");
+            conn.Open();
+            return conn;
+        }
+    }
+}
